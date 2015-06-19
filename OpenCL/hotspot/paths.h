66d41ef31a1ff2da65c1f6b634c67248d162df4a
stardica <stardica@stardica-hec313-32bit>
2015-06-19 16:38:12 -0400

    cleaning up hotspot

--- OpenCL/hotspot/paths.h
+++ OpenCL/hotspot/paths.h
@@ -1,1 +1,15 @@
-char kernel_binary[250] = "/home/stardica/Dropbox/WorkingRodiniaBenchmarks/OpenCL/hotspot/hotspot_kernel.cl.bin.GPU";
+/*
+ * paths.h
+ *
+ *  Created on: Jun 19, 2015
+ *      Author: stardica
+ */
+
+#ifndef PATHS_H_
+#define PATHS_H_
+
+
+char kernel_binary[250] = "/home/stardica/Dropbox/CDA7919DoctoralResearch/Rodinia_Benchmarks/OpenCL/hotspot/hotspot_kernel.cl.bin.GPU";
+
+
+#endif /* PATHS_H_ */
