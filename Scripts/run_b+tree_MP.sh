3768468e94c4b474d0035de37e7e7c54f4535f07
stardica <stardica@gmail.com>
2015-06-21 09:52:15 -0400

    cleaned up b+tree

--- Scripts/run_b+tree_MP.sh
+++ Scripts/run_b+tree_MP.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-source ./scripts/paths.sh
+source /home/stardica/Dropbox/CDA7919DoctoralResearch/Rodinia_Benchmarks/Scripts/paths.sh
 
 echo " "
 echo "---Running B+Tree OpenMP on simulator---"
 for i in 1 2 4
 do
-$m2s_cgm_path/m2s-cgm --x86-sim detailed --x86-config $config_file_path/$cpu_config_file_name $benchmark_path/OpenMP/b+tree/b+tree.out core $i file $benchmark_path/data/b+tree/mil.txt command $benchmark_path/data/b+tree/command.txt
+$m2s_cgm_path/m2s-cgm --x86-sim detailed --x86-config $config_file_path/$cpu_config_file_name --si-sim detailed --si-config $config_file_path/$gpu_config_file_name --mem-config $config_file_path/$mem_config_file_name $benchmark_path/OpenMP/b+tree/b+tree core $i file $benchmark_path/data/b+tree/mil.txt command $benchmark_path/data/b+tree/command.txt
 done
-
-#--mem-config /home/stardica/Desktop/m2s-cgm/src/config/cgm-config.ini
