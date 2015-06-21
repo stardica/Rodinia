8bc0103d46def9dc1a7aad51b9d0a62ef1f07f4e
stardica <stardica@gmail.com>
2015-06-21 09:32:14 -0400

    cleand up lavalmd

--- lavamd.sh
+++ lavamd.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-./scripts/run_lavamd_MP.sh
-#./scripts/run_streamcluster_CL.sh
+
+source /home/stardica/Dropbox/CDA7919DoctoralResearch/Rodinia_Benchmarks/Scripts/paths.sh
+
+$scripts_path/run_lavamd_MP.sh
+#$scripts_path/run_streamcluster_CL.sh
