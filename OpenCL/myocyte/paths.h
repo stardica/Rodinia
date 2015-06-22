30e963f1f2e6d077652b64aaf5cbb72a345ef678
stardica <stardica@stardica-hec313-32bit>
2015-06-22 17:24:46 -0400

    cleaned up myocyteCL

--- OpenCL/myocyte/paths.h
+++ OpenCL/myocyte/paths.h
@@ -1,8 +1,16 @@
+/*
+ * paths.h
+ *
+ *  Created on: Jun 22, 2015
+ *      Author: stardica
+ */
 
 #ifndef PATHS_H_
 #define PATHS_H_
 
+
 char param_file[250] = "/home/stardica/Dropbox/CDA7919DoctoralResearch/Rodinia_Benchmarks/data/myocyte/params.txt";
 char y_file[250] = "/home/stardica/Dropbox/CDA7919DoctoralResearch/Rodinia_Benchmarks/data/myocyte/y.txt";
+
 
 #endif /* PATHS_H_ */
--- myocyte.sh
+++ myocyte.sh
@@ -3,4 +3,4 @@
 source /home/stardica/Dropbox/CDA7919DoctoralResearch/Rodinia_Benchmarks/Scripts/paths.sh
 
 $scripts_path/run_myocyte_MP.sh
-#$scripts_path/run_myocyte_CL.sh
+$scripts_path/run_myocyte_CL.sh
