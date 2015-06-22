f484182768fdc8e6b7f27c60638e5154f3512869
stardica <stardica@stardica-hec313-32bit>
2015-06-22 17:02:19 -0400

    cleaned up b+treeCL

--- b+tree.sh
+++ b+tree.sh
@@ -3,4 +3,4 @@
 source /home/stardica/Dropbox/CDA7919DoctoralResearch/Rodinia_Benchmarks/Scripts/paths.sh
 
 $scripts_path/run_b+tree_MP.sh
-#$scripts_path/run_pathfinder_CL.sh
+$scripts_path/run_b+tree_CL.sh
