cd0dfff93ddb02c27c3fffa1e3cefa58335b3a0f
stardica <stardica@stardica-hec-u14-64bit>
2017-01-16 21:17:59 -0500

    sync

--- OpenMP/backprop/backprop_kernel.c
+++ OpenMP/backprop/backprop_kernel.c
@@ -63,21 +63,12 @@ void bpnn_train_kernel(BPNN *net, float *eo, float *eh)
   //simulator start stats collection
   syscall(BEGIN_PARALLEL_SECTION);
 
-  //p_start = rdtsc();
-
   bpnn_layerforward(net->input_units, net->hidden_units,net->input_weights, in, hid);
   bpnn_layerforward(net->hidden_units, net->output_units, net->hidden_weights, hid, out);
   bpnn_output_error(net->output_delta, net->target, net->output_units, out, &out_err);
   bpnn_hidden_error(net->hidden_delta, hid, net->output_delta, out, net->hidden_weights, net->hidden_units, &hid_err);  
   bpnn_adjust_weights(net->output_delta, out, net->hidden_units, hid, net->hidden_weights, net->hidden_prev_weights);
   bpnn_adjust_weights(net->hidden_delta, hid, net->input_units, in, net->input_weights, net->input_prev_weights);
-
-  //simulator stop stats collection
-
-  //p_end = rdtsc();
-
- //unsigned long long p_time = p_end - p_start;
- //printf("p_sections cycles %llu\n", p_time);
 
   syscall(END_PARALLEL_SECTION);
 
